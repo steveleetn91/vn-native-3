67885ffcc59bb54ce97a2bedfcdf8208a6896b6a
CEO <hoang.le.tn91@gmail.com>
2021-11-25 19:31:12 +0700

    Prepare release beta

--- pages/index.ts
+++ pages/index.ts
@@ -1,5 +1,6 @@
 import HTMLPageInterFace from 'vnnative3-webview/dist/HTMLPage'
 import VnNative3HTMLElement from 'vnnative3-webview/dist/HTMLElement/index';
+import VnNative3Redirect from 'vnnative3-redirect/dist/index'
 export default class IndexPage implements HTMLPageInterFace {
     state = {
         title : "",
@@ -15,10 +16,14 @@ export default class IndexPage implements HTMLPageInterFace {
     public afterRender() : void {
         
     }
+    public test(){
+        // 404 
+        return (new VnNative3Redirect).to('home/test2');
+    }
     public render() : string {
         return (new VnNative3HTMLElement).make(`
-        <div onclick="IndexPage.notifi()">
-            <h1>{{title}}</h1>
+        <div>
+            <h1 onclick="IndexPage.test()">{{title}}</h1>
             <p>{{slogan}}</p>
         </div>
         `,this.state);
