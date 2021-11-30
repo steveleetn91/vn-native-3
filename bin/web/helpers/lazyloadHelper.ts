2c4ec094b33cb628b7f62544f2c68f5c986a4ffa
CEO <hoang.le.tn91@gmail.com>
2021-12-01 04:59:39 +0700

    Remove lazyload helper

--- bin/web/helpers/lazyloadHelper.ts
+++
@@ -1,9 +0,0 @@
-export default class lazyLoadHelper {
-    name : any;
-    setName(name : string) : void {
-        this.name = name;
-    }
-    getName() : any {
-        return this.name;
-    }
-}
