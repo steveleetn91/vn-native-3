6114280416d1562b220ef9a6e839d1336dcc326e
Steve Lee <55998182+steveleetn91@users.noreply.github.com>
2022-02-16 23:35:25 +0700

    Update Production.java

--- bin/android/java/Production.java
+++ bin/android/java/Production.java
@@ -2,33 +2,6 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import com.example.myapplication.MyWebViewClient;
-
-public class MainActivity extends AppCompatActivity {
-
-    private Context activityContext;
-    private MyWebViewClient MyWebViewClient;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.getSettings().setUseWideViewPort(true);
-        myWebView.setWebViewClient(new MyWebViewClient());
-        myWebView.setWebChromeClient(new WebChromeClient());
-        myWebView.loadUrl("file:///android_asset/index.html");
-    }
-}
-package com.example.myapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
